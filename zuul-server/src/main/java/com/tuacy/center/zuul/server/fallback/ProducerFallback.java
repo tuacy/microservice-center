937e87fcdb495b6fef917460d95b2203b3c67c77
tuacy <1007178106@qq.com>
2018-09-18 22:22:11 +0800

    logback

---
+++ zuul-server/src/main/java/com/tuacy/center/zuul/server/fallback/ProducerFallback.java
@@ -0,0 +1,78 @@
+package com.tuacy.center.zuul.server.fallback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 当我们的后端服务出现异常的时候，我们不希望将异常抛出给最外层，期望服务可以自动进行一降级。Zuul给我们提供了这样的支持。当某个服务出现异常时，直接返回我们预设的信息。
+ */
+@Component
+public class ProducerFallback implements FallbackProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerFallback.class);
+
+    /**
+     * 指定要处理的 serviceId
+     *
+     * @return serviceId
+     */
+    @Override
+    public String getRoute() {
+        return null;
+    }
+
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause != null && cause.getCause() != null) {
+            String reason = cause.getCause().getMessage();
+            LOG.info("Excption {}",reason);
+        }
+        return fallbackResponse();
+    }
+}
--- zuul-server/src/main/java/com/tuacy/center/zuul/server/filter/PreRequestFilter.java
+++ zuul-server/src/main/java/com/tuacy/center/zuul/server/filter/PreRequestFilter.java
@@ -1,11 +1,19 @@
 package com.tuacy.center.zuul.server.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 
+/**
+ * PRE： 这种过滤器在请求被路由之前调用。我们可利用这种过滤器实现身份验证、在集群中选择请求的微服务、记录调试信息等
+ * ROUTING：这种过滤器将请求路由到微服务。这种过滤器用于构建发送给微服务的请求，并使用Apache HttpClient或Netfilx Ribbon请求微服务。
+ * POST：这种过滤器在路由到微服务以后执行。这种过滤器可用来为响应添加标准的HTTP Header、收集统计信息和指标、将响应从微服务发送给客户端等。
+ * ERROR：在其他阶段发生错误时执行该过滤器。 除了默认的过滤器类型，Zuul还允许我们创建自定义的过滤器类型。例如，我们可以定制一种STATIC类型的过滤器，直接在Zuul中生成响应，而不将请求转发到后端的微服务。
+ */
 @Component
 public class PreRequestFilter extends ZuulFilter {
 
@@ -13,6 +21,7 @@ public class PreRequestFilter extends ZuulFilter {
 
     @Override
     public String filterType() {
+        LOG.debug("abc");
         return "pre";
     }
 
@@ -28,7 +37,8 @@ public class PreRequestFilter extends ZuulFilter {
 
     @Override
     public Object run() {
-
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
         return null;
     }
 }
